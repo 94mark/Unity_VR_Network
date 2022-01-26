9cb925e2fcd284774345a3a7f1c6d5fb71c45b18
94mark <90877724+94mark@users.noreply.github.com>
2022-01-26 11:40:27 +0900

    Update ConnManager.cs
    Phton API 사용
    Pun.Realtime 네임스페이스 추가

---
+++ VR_Network/Assets/Scripts/ConnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Photon API를 사용하기 위한 네임스페이스
+using Photon.Pun;
+using Photon.Realtime;
+
+//네트워크 처리 클래스
+public class ConnManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
