1f9fd86b5e51931f98b546cb43b6717dfd76b88e
94mark <90877724+94mark@users.noreply.github.com>
2022-01-26 12:22:33 +0900

    Update ConnManager.cs
    Start 함수에서 포톤 접속 설정 및 서버 접속

--- VR_Network/Assets/Scripts/ConnManager.cs
+++ VR_Network/Assets/Scripts/ConnManager.cs
@@ -8,10 +8,20 @@ using Photon.Realtime;
 //네트워크 처리 클래스
 public class ConnManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    //시작할 때 서버에 접속
     void Start()
     {
-        
+        PhotonNetwork.GameVersion = "0.1";
+
+        //게임에서 사용할 사용자의 이름을 랜덤으로 정한다
+        int num = Random.Range(0, 1000);
+        PhotonNetwork.NickName = "Player " + num;
+
+        //게임에 참여하면 마스터 클라이언트가 구성한 씬에 자동으로 동기화하도록 한다
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        //서버 접속
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     // Update is called once per frame
