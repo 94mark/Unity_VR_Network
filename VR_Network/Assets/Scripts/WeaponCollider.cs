dc2cba72aa1512b7e97346dc29862c9d3d0e802a
94mark <90877724+94mark@users.noreply.github.com>
2022-01-26 23:05:20 +0900

    Update WeaponCollider.cs
    무기 콜라이더 활성화/비활성화

---
+++ VR_Network/Assets/Scripts/WeaponCollider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollider : MonoBehaviour
+{
+    public BoxCollider weaponCol;
+
+    void Start()
+    {
+        //무기의 충돌 영역을 비활성화한다
+        DeactivateCollider();
+    }
+
+    //콜라이더 활성화 함수
+    public void ActiveCollider()
+    {
+        weaponCol.enabled = true;
+    }
+
+    //콜라이더 비활성화 함수
+    public void DeactivateCollider()
+    {
+        weaponCol.enabled = false;
+    }
+}
